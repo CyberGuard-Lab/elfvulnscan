c5493f04d3a1d38da723e0a1a70492edb2cba86e
hieuvd341 <vuduchieu1611@gmail.com>
2025-06-29 01:14:18 +0700

    add double free program

---
+++ binary_source/heap_dbf.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void free_but_from_other_function(char *p) {
+    free(p);
+}
+
+int main() {
+    char *a = malloc(0x40);
+    char *b = malloc(0x40);
+    char *ptr = NULL;
+
+    strcpy(a, "this is chunk a");
+    strcpy(b, "this is chunk b");
+
+    int choice;
+    printf("Enter 1 or 2: ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        ptr = a;
+    } else {
+        ptr = b;
+    }
+
+    free_but_from_other_function(ptr);  
+
+    if (choice == 1) {
+        free(a);  
+    }
+
+    return 0;
+}
