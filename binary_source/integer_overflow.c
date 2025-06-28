b352a5ba70a3fa5ba12009cd37adb9c386da80f3
hieuvd341 <vuduchieu1611@gmail.com>
2025-06-29 01:19:51 +0700

    chìm

--- binary_source/integer_overflow.c
+++ binary_source/integer_overflow.c
@@ -3,24 +3,30 @@
 #include <stdint.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int sum = 10;
     int tmp;
     int choice;
     printf("Enter a number: ");
     scanf("%d", &choice);
-    while(choice == 1) {
+    while (choice == 1)
+    {
         printf("Enter a number: ");
         scanf("%d", &tmp);
-        if (tmp < 0) {
+        if (tmp < 0)
+        {
             printf("Negative number detected, exiting...\n");
             exit(1);
         }
         sum = sum + tmp;
-        if(sum == 9) {
+        if (sum == 9)
+        {
             printf("You have reached the magic number 9!\n");
             system("/bin/sh");
-        } else {
+        }
+        else
+        {
             printf("Current sum: %d\n", sum);
         }
         printf("Enter 1 to continue or 0 to exit: ");
