ab18187e60227a108f4cef1201e664c519705f1c
hieuvd341 <vuduchieu1611@gmail.com>
2025-06-29 01:06:56 +0700

    add type confusion program

---
+++ binary_source/type_confusion.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct dts
+{
+    char name[64];
+    int age;
+    int isGei;
+};
+
+struct bvh {
+    char name[72];
+};
+
+void shell()
+{
+    system("/bin/sh");
+}
+
+int main() {
+    struct dts *dts = malloc(sizeof(struct dts));
+    strcpy(dts->name, "dinhthaison");
+    dts->age = 18;
+    dts->isGei = 0;
+    struct bvh *b = (struct bvh *)dts;
+    fgets(b->name, sizeof(b->name), stdin);
+    if(dts->isGei != 0 ) {
+        printf("You are a gei\n");
+        shell();
+    } else {
+        printf("You are not a gei\n");
+    }
+    return 0;
+}
